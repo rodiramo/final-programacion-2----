c167ed97bbe0c96c100ef6bf35bf5f217a65a2b5
rodiramo <82394566+rodiramo@users.noreply.github.com>
2023-08-01 03:16:54 +0200

    as

--- sitio/admin/pages/purchases.php
+++ sitio/admin/pages/purchases.php
@@ -1,33 +1,29 @@
 <?php
 use Classes\Models\Cart_Finally;
-
-$userId = $_GET['id'];
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 $cart = (new Cart_Finally)->viewById();
-
-
-        ?>
-        <section class="container">
-            <h1 class="mb-1">Purchases</h1>
-          
-            <h2>Latest purchases</h2>
-        
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Price</th>
-                        <th>Cant</th>
-                    </tr>
-                </thead>
-                <tbody>
-                       <tr>
-                            <td><?=$cart->getFinalPrice(); ?></td>
-                            <td><?=$cart->getFecha(); ?></td>
-                        </tr>
-                </tbody> 
-            </table>
-        </section>
-        <?php
-    
 ?>
+
+<section class="container">
+    <h1 class="mb-1">Purchases</h1>
+    <h2>Latest purchases</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Cant</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($cart as $item): ?>
+    <?php echo "<pre>"; print_r($item); echo "</pre>"; ?>
+            <tr>
+                <td><?=$item->getTitle(); ?></td>
+                <td><?=$item->getPrice(); ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody> 
+    </table>
+</section>
